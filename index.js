4cc59a8d8a0df886b3599d0d1360021e8d5fc546
mmwandera <mmwandera74@gmail.com>
2023-11-02 22:56:32 +0300

    added cocktail roulette feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded' , function(){
     const popularDrinksList = document.getElementById('popular-cocktail-list');
     const latestDrinksList = document.getElementById('latest-cocktail-list');
     const drinkDetails = document.querySelector('.right');
+    const cocktailRoulette = document.querySelector('.randomize-button');
 
     // displays drink details on the right side
     function fetchAndDisplayDrinkDetails(id) {
@@ -117,7 +118,52 @@ document.addEventListener('DOMContentLoaded' , function(){
     const latestDrinksIdArray = [178370,178369,17191,178367,178366,178365,178364,178363]
     fetchLatestDrinkList(latestDrinksIdArray)
 
-    // Search functionality
+    // cocktail roulette feature
+    cocktailRoulette.addEventListener('click', () => {
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data);
+                let ingredientsList = '';
+    
+                for (let i = 1; i <= 15; i++) {
+                    const ingredientKey = 'strIngredient' + i;
+                    const measureKey = 'strMeasure' + i;
+    
+                    if (data.drinks[0][ingredientKey] !== null && data.drinks[0][ingredientKey] !== '') {
+                        ingredientsList += `<li>${data.drinks[0][ingredientKey]}</li>`;
+                    }
+                }
+    
+                drinkDetails.innerHTML =
+                    `
+                    <h2 class="cocktail-name">${data.drinks[0].strDrink}</h2>
+                    <div class="cocktail-image">
+                        <!-- Image of the cocktail -->
+                        <img src="${data.drinks[0].strDrinkThumb}" style ="height:300px; width=300px" />
+                    </div>
+                    <h3 class="ingredients-heading">Ingredients</h3>
+                    <div class="ingredients">
+                        <!-- Ingredient cards appended dynamically -->
+                        <ul>
+                            ${ingredientsList}
+                        </ul>
+                    </div>
+                    <h3 class="instructions-heading">Instructions</h3>
+                    <p class="instructions-text">
+                        <!-- Instructions fetched from the server -->
+                        ${data.drinks[0].strInstructions}
+                    </p>
+                    <h3 class="glass-heading">Glass</h3>
+                    <p class="glass-text">
+                        <!-- Glass fetched from the server -->
+                        ${data.drinks[0].strGlass}
+                    </p>
+                    `;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    });
+    
 
 
 })
